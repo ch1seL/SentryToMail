0b114cae2c1ede83b372e749dc738a387b6a0b00
Stanislav Perekrestov <sperekrestov@appulate.com>
2018-10-23 12:18:04 +0300

    Fixes issue #1

--- src/SentryToMail.Utils/FileCollection.cs
+++ src/SentryToMail.Utils/FileCollection.cs
@@ -6,9 +6,10 @@ using Newtonsoft.Json;
 
 namespace SentryToMail.Utils {
 	public class FileCollection<T> where T : class, ISerializable, new() {
-		private static readonly Encoding Encoding = Encoding.Unicode;
 		private readonly string _filePath;
 		private T _collection;
+		private static readonly JsonSerializerSettings _serializerSettings = new JsonSerializerSettings
+			{ Formatting = Formatting.Indented };
 
 		public FileCollection(string filePath, T collection = null) {
 			_collection = collection ?? new T();
@@ -24,20 +25,39 @@ namespace SentryToMail.Utils {
 		}
 
 		public TR Update<TR>(Func<T, TR> func) {
-			byte[] file = File.ReadAllBytes(_filePath);
-			_collection = JsonConvert.DeserializeObject<T>(Encoding.GetString(file)) ?? _collection;
+			_collection = ReadJsonFromFile(_filePath) ?? _collection;
 
 			TR result = func(_collection);
 
-			file = Encoding.GetBytes(JsonConvert.SerializeObject(_collection, Formatting.Indented));
-			File.WriteAllBytes(_filePath, file);
+			WriteJsonToFile(_filePath, _collection);
 
 			return result;
 		}
 
+		private T ReadJsonFromFile(string filePath) {
+			using (FileStream stream = File.OpenRead(filePath)) {
+				using (var reader = new StreamReader(stream)) {
+					JsonSerializer serializer = JsonSerializer.CreateDefault();
+					var jsonTextReader = new JsonTextReader(reader);
+					return serializer.Deserialize<T>(jsonTextReader);
+				}
+			}
+		}
+
+		private void WriteJsonToFile(string filePath, T obj) {
+			using (FileStream stream = File.OpenWrite(filePath)) {
+				using (var reader = new StreamWriter(stream)) {
+					JsonSerializer serializer = JsonSerializer.Create(_serializerSettings);
+					var jsonTextWriter = new JsonTextWriter(reader);
+					serializer.Serialize(jsonTextWriter, obj);
+					jsonTextWriter.Flush();
+				}
+			}
+		}
+
 		public T PeekAll() {
-			byte[] file = File.ReadAllBytes(_filePath);
-			_collection = JsonConvert.DeserializeObject<T>(Encoding.GetString(file)) ?? _collection;
+			T jsonFromFile = ReadJsonFromFile(_filePath);
+			_collection = jsonFromFile ?? _collection;
 			return _collection;
 		}
 	}
